9622821ad0628279e24909973e4e79e1ab6c9bd8
Matt Dobson <mdobson4@gmail.com>
2013-08-20 14:10:36 -0400

    First test completed.

--- test/gzip.js
+++ test/gzip.js
@@ -1,7 +1,40 @@
-var assert = require("assert");
+var assert = require("assert"),
+	Stream = require("stream"),
+	util = require("util"),
+	argo = require("argo"),
+	argo_gzip = require("../");
+
+//Mocks
+
+function Request(){
+	this.headers = {};
+	Stream.call(this);
+}
+util.inherits(Request, Stream);
+
+function Response() {
+  this.headers = {};
+  this.statusCode = 0;
+  this.body = '';
+  this.writable = true;
+  Stream.call(this);
+}
+util.inherits(Response, Stream);
+
+function _getEnv() {
+  return { 
+    request: new Request(),
+    response: new Response(),
+    target: {},
+    argo: {}
+  };
+}
 
 describe("argo-gzip", function(){
-	it("evaluates true as true",function(){
-		assert.ok(true===true);
-	});	
+	it("has a name and install property", function(){
+		var server = argo();
+		var gzip_package = argo_gzip(server);
+		assert.ok(gzip_package.name === "gzip compression");
+		assert.ok(gzip_package.install);
+	});
 });
